b48c0bf8f89c41f5a9db5550d13e20fc5ad4cc0c
antoine <antoine.guingand@wanadoo.fr>
2023-03-15 11:18:31 +0100

    Fix bash

--- check-ray-calls.sh
+++ check-ray-calls.sh
@@ -1,9 +1,8 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 RESULTS=`grep -r --include "*.php" "\W@\?ray(" ./app ./resources/views ./database ./routes ./tests`
-LINE_COUNT=`echo $RESULTS | xargs | wc -l`
 
-if [ $LINE_COUNT -gt 0 ]; then
+if [ ! -z $RESULTS ]; then
   echo "found some ray() calls..."
   echo $RESULTS
   exit 1
