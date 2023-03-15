1d9af17abcd869247d2a8c30c74f3696c97db5db
antoine <antoine.guingand@wanadoo.fr>
2023-03-15 11:55:30 +0100

    Typo

--- check-ray-calls.sh
+++ check-ray-calls.sh
@@ -3,9 +3,9 @@
 RESULTS=`grep -r --include "*.php" "\W@\?ray(" ./app ./resources/views ./database ./routes ./tests`
 
 if [ ! -z "$RESULTS" ]; then
-  echo "found some ray() calls..."
+  echo "Found some ray() calls..."
   echo "${RESULTS}"
   exit 1
 else
-  echo "no ray() calls found !"
+  echo "No ray() calls found !"
 fi
